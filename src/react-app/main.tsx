920d2973df5f9d835b8516a4e6c80664bf0049ec
Scott Odle <scott@sjodle.com>
2025-09-05 16:52:18 -0600

    init

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import Privacy from "./Privacy.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy" element={<Privacy />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,54 @@
 import { Hono } from "hono";
+import { trimTrailingSlash } from "hono/trailing-slash";
+
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.use(trimTrailingSlash());
+app.get("/api", (c) =>
+  c.json({
+    ios: {
+      yorkstreet: {
+        latestShapefile: {
+          url: "/api/shapefiles/yorkstreet/1",
+          effectiveDate: "2025-09-05",
+          sha256:
+            "3ba314db2b2aa1b2f194e44d3396a72eaf82d1077376d8dbd09c138a705d768b",
+        },
+        latestCategoryFile: {
+          url: "/api/category_files/yorkstreet/1",
+          effectiveDate: "2025-09-05",
+          sha256:
+            "30b3fc44a4e9aaf828c1d5d196b7e26a8eefda9392d2f854b4fa84c0dec37ff1",
+        },
+      },
+    },
+  })
+);
+
+const yorkStreetShapefiles = [await import("./shapefiles/yorkstreet/01.json")];
+const yorkStreetCategoryFiles = [
+  await import("./category_files/yorkstreet/01.json"),
+];
+
+app.get("/api/:fileType/:location/:id", (c) => {
+  let fileType = c.req.param("fileType");
+  let location = c.req.param("location");
+  let fileIndex = parseInt(c.req.param("id")) - 1;
+  if (
+    location == "yorkstreet" &&
+    fileType == "shapefiles" &&
+    fileIndex < yorkStreetShapefiles.length
+  ) {
+    return c.json(yorkStreetShapefiles[fileIndex]);
+  }
+  if (
+    location == "yorkstreet" &&
+    fileType == "category_files" &&
+    fileIndex < yorkStreetCategoryFiles.length
+  ) {
+    return c.json(yorkStreetCategoryFiles[fileIndex]);
+  }
+  return c.notFound();
+});
 
 export default app;
