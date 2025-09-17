ede25198a33e25a82ca9fe69f42bf2d03ff284c2
Scott Odle <scott@sjodle.com>
2025-09-17 08:34:03 -0600

    add android source code

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -16,23 +16,28 @@ function App() {
       <h1>Lost in the Gardens</h1>
       <h2>Explore the Denver Botanic Gardens with confidence.</h2>
       <div>
-        <a href="https://apps.apple.com/us/app/lost-in-the-gardens/id6751761489">
-          <img src={iosAppStoreButton} />
-        </a>
+        <h3>Get the app</h3>
+        <p>
+          <a href="https://apps.apple.com/us/app/lost-in-the-gardens/id6751761489">
+            <img src={iosAppStoreButton} />
+          </a>
+        </p>
       </div>
       <div>
-        Copyright ©️ 2025 Scott Odle | <Link to="/privacy">Privacy Policy</Link>{" "}
-        |{" "}
-        <a href="https://github.com/sodle/lost_in_the_gardens" target="_blank">
-          Source Code (iOS)
-        </a>{" "}
-        |{" "}
-        <a
-          href="https://github.com/sodle/lostinthegardens-backend"
-          target="_blank"
-        >
-          Source Code (website/backend)
-        </a>
+        <h3>Source code</h3>
+        <p>
+          <a href="https://github.com/sodle/lost_in_the_gardens">iOS app</a> |{" "}
+          <a href="https://github.com/sodle/lostinthegardens-android">
+            Android app
+          </a>{" "}
+          (work in progress) |{" "}
+          <a href="https://github.com/sodle/lostinthegardens-backend">
+            Website/Backend
+          </a>
+        </p>
+      </div>
+      <div>
+        Copyright ©️ 2025 Scott Odle | <Link to="/privacy">Privacy Policy</Link>
       </div>
     </>
   );
